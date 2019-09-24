074c117850cebbda9fe451432b75a5194d95090c
Jeff <jeff.uca@gmail.com>
2019-09-24 10:14:16 +0200

    Added: Function _get_items to refactor Added: Search function Change: List function to incorporate _get_items function, and minor changes.

--- apigoogledrive/apidrive.py
+++ apigoogledrive/apidrive.py
@@ -15,14 +15,28 @@ class GoogleDriveV3(auth.Auth):
         creds = super().get_credentials()
         self.service = build('drive', 'v3', credentials=creds)
 
+    def _get_items(self, query=None, pagesize=None, fields="nextPageToken, files(id, name)"):
+        """
+        Get the items of the Google Drive
+        :parameter query:
+        :parameter pagesize:
+        :parameter fields:
+        :return items: All the objects (folders and files) of your Google Drive
+        """
+
+        results = self.service.files().list(q=query, pageSize=pagesize, fields=fields).execute()
+
+        return results
+
     def list_files(self, pagesize=10, fields="nextPageToken, files(id, name)"):
+        # TODO search what fields is it.
         """
         List the files of your google drive Globally
+        :param pagesize: Shows the n elements. Default value = 10.
+        :param fields:
         """
-        # Call the Drive v3 API
-        results = self.service.files().list(
-            pageSize=pagesize, fields=fields).execute()
-        items = results.get('files', [])
+        response = self._get_items(None, pagesize, fields)
+        items = response.get('files', [])
 
         if not items:
             print('No files found.')
@@ -31,11 +45,22 @@ class GoogleDriveV3(auth.Auth):
             for item in items:
                 print(u'{0} ({1})'.format(item['name'], item['id']))
 
-    def create_directory(self, path, name):
-        # TODO implement
-        pass
+        return items
+
+    def create_directory(self, path, name='My Directory'):
+
+        file_metadata = {
+            'name': name,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+
+        file = self.service.files().create(body=file_metadata,
+                                            fields='id').execute()
+        print('File ID: %s' % file.get('id'))
 
     def create_file(self, path=None, name='My Report'):
+
+        # TODO parametrize correctly
 
         # application / vnd.google - apps.document google docs
         # application/vnd.google-apps.file 	Google Drive file
@@ -48,33 +73,50 @@ class GoogleDriveV3(auth.Auth):
         }
 
         file = self.service.files().create(body=file_metadata,
-                                            media_body=None,
                                             fields='id').execute()
         print('File ID: %s' % file.get('id'))
 
     def upload_directory(self, path, name):
 
+        # TODO implement: change mimetype for folder
+        pass
         # application / vnd.google - apps.document google docs
         # application/vnd.google-apps.file 	Google Drive file
         # application/vnd.google-apps.folder 	Google Drive folder
         # application/vnd.google-apps.spreadsheet 	Google Sheets
 
-        file_metadata = {
-            'name': name,
-            'mimeType': 'application/vnd.google-apps.document'
-        }
-        media = MediaFileUpload(path,
-                                mimetype='text/doc',
-                                resumable=True)
-
-        file = self.service.files().create(body=file_metadata,
-                                            media_body=media,
-                                            fields='id').execute()
-        print('File ID: %s' % file.get('id'))
-
+        # file_metadata = {
+        #     'name': name,
+        #     'mimeType': 'application/vnd.google-apps.document'
+        # }
+        # media = MediaFileUpload(path,
+        #                         mimetype='text/doc',
+        #                         resumable=True)
+        #
+        # file = self.service.files().create(body=file_metadata,
+        #                                     media_body=media,
+        #                                     fields='id').execute()
+        # print('File ID: %s' % file.get('id'))
 
     def upload_file(self):
-        # TODO implement
+        # TODO implement, change mimetype to generic upload or concrete upload by files
+        # application / vnd.google - apps.document google docs
+        # application/vnd.google-apps.file 	Google Drive file
+        # application/vnd.google-apps.folder 	Google Drive folder
+        # application/vnd.google-apps.spreadsheet 	Google Sheets
+
+        # file_metadata = {
+        #     'name': name,
+        #     'mimeType': 'application/vnd.google-apps.document'
+        # }
+        # media = MediaFileUpload(path,
+        #                         mimetype='text/doc',
+        #                         resumable=True)
+        #
+        # file = self.service.files().create(body=file_metadata,
+        #                                     media_body=media,
+        #                                     fields='id').execute()
+        # print('File ID: %s' % file.get('id'))
         pass
 
     def give_permissions(self, file, user):
@@ -88,3 +130,28 @@ class GoogleDriveV3(auth.Auth):
     def share_with(self, file, user):
         # TODO implement
         pass
+
+    def search(self, name_file):
+        """
+        :param name_file: name of the file to search
+        :return: a dictionary of dictionaries
+        """
+
+        page_token = None
+
+        files = []
+
+        while True:
+
+            response = self._get_items("name='{}'".format(name_file),page_token,
+                                       'nextPageToken, files(id, name, createdTime)',
+                                       )
+
+            for file in response.get('files', []):
+                files.append({'id': file.get('id'), 'name': file.get('name'), 'date_creation': file.get('createdTime')})
+
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+
+        return files
--- testing/main.py
+++ testing/main.py
@@ -7,9 +7,14 @@ if __name__ == '__main__':
 
     GD = apidrive.GoogleDriveV3('https://www.googleapis.com/auth/drive')
 
-    GD.list_files()
 
-    GD.create_file('hola')
+    # GD.create_file('nopath')
+    #
+    # GD.create_directory('nopath')
 
 
     print(GD.SCOPES)
+
+    print(GD.search('My Report'))
+
+    # GD.list_files()
